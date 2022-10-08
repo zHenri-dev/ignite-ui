04ab5e0b06bc725103ba3edce22b81649598c784
zHenri-dev <henriquegasparotte@gmail.com>
2022-10-07 22:25:01 -0300

    initial commit

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,26 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/zhenri-dev.png',
+    alt: 'zHenri Developer',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,51 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam illo nostrum fugit, vero soluta minima impedit consectetur numquam quasi, explicabo illum dolores nemo totam dolore, saepe non aspernatur excepturi provident.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o Heading sempre será um `p`, mas podemos alterar isso com a propriedade `as`.',
+      },
+    },
+  },
+}
