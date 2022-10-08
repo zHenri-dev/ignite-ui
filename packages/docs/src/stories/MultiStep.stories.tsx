f30fe7a5f0bb4ac08a1e5532b20373a7f1e3eb9c
zHenri-dev <henriquegasparotte@gmail.com>
2022-10-07 23:05:39 -0300

    update packages name

--- packages/docs/src/stories/MultiStep.stories.tsx
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -1,5 +1,5 @@
 import { StoryObj, Meta } from '@storybook/react'
-import { Box, MultiStep, MultiStepProps } from '@ignite-ui/react'
+import { Box, MultiStep, MultiStepProps } from '@zhenri-ignite-ui/react'
 
 export default {
   title: 'Form/Multi Step',
